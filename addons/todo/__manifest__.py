300282f166ae30159d6c49b030a9bc147fedd3a9
eisan97 <thineisan2021@gmail.com>
2023-12-09 21:01:12 +0630

    update code

---
+++ addons/todo/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name':'Todo',
+    'author':'Thin Ei San',
+    'depends':[],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/todo.xml',
+    ],
+    'license':'LGPL-3'
+
+}
