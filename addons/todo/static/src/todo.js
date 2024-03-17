1cb3bb1575fd9c85a2ac90de9bae2451c640b81a
OdooDev9 <umgodoodev9@gmail.com>
2024-03-17 21:42:50 +0630

    get all task , ORM services

--- addons/todo/static/src/todo.js
+++ addons/todo/static/src/todo.js
@@ -1,9 +1,31 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-const {Component} = owl;
+const {Component,onWillStart,useState} = owl;
 
 class TodoList extends Component{
+    setup(){
+        this.state = useState({
+            tasklist:[
+                // {"id":"1","name":"Sample Task 1","complete":false},
+                // {"id":"2","name":"Sample Task 2","complete":false},
+                // {"id":"3","name":"Sample Task 3","complete":true},
+                // {"id":"4","name":"Sample Task 4","complete":false},
+                // {"id":"5","name":"Sample Task 5","complete":false},
+            ]
+        })
+        onWillStart(async() =>{
+            await this.getAllTask()
+        })
+
+    }
+
+    async getAllTask(){
+        const orm =  this.env.services.orm
+        // var allTaskList = await orm.searchRead('to.do',[],['name','complete'])
+        // console.log(allTaskList)
+        this.state.tasklist = await orm.searchRead('to.do',[],['name','complete'])
+    }
 
 }
 TodoList.template = "todo.todo_list";
